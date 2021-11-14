cbd0075bce988b80bd2e83c82c6a8b9354f93d23
shrey19702 <shrey19702@gmail.com>
2021-11-14 22:52:32 +0530

    add css , navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav id="Navbar">
+            <div id="nav-heading">Shrey Gupta</div>
+
+            <div className="nav-links">
+                <a href="/" className="nav-list-high">home</a>
+                <a href="/about">about</a>
+                <a href="/contact">contact</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
