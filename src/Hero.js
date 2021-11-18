c181bc574072c41bdd3bea10d681c5c5c387c9ef
shrey19702 <shrey19702@gmail.com>
2021-11-18 20:18:56 +0530

    add background image

--- src/Hero.js
+++ src/Hero.js
@@ -4,7 +4,7 @@ const HeroPage = () => {
         <div className="hero-page">
             <div className="hero-content">
                 <h1 className="hero-title">Nice to see you.</h1>
-                <p className="hero-intro">&#128075; Hi! My name is Shrey and I'm a aspiring full stack developer and User Experience Designer Studing at DTU.</p>
+                <p className="hero-intro">&#128075; Hi! My name is Shrey and I'm a aspiring full stack developer and user experience designer studing at DTU.</p>
             </div>
         </div>
     );
