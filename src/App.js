f778d5706f9bb0f8b80a4dfef76606c364fa601a
shrey19702 <shrey19702@gmail.com>
2021-11-19 18:15:42 +0530

    add routing ,add state to navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import Navbar from './Navbar'
-import HeroPage from './Hero'
-import Projects from './Projects'
-import Footer from './Footer'
+import Navbar from './Navbar';
+import HeroPage from './Hero';
+import Projects from './Projects';
+import Footer from './Footer';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <div className="main-page">
-        <Navbar/>
-        <HeroPage/>
+    <Router>
+      <div className="App">
+        <div className="main-page">
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<HeroPage/>} />
+          </Routes>
+        </div>
+        <Projects/>
+        <Footer/>
       </div>
-      <Projects/>
-      <Footer/>
-    </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,39 +1,49 @@
-import { useState } from "react";
+import {useState, useEffect } from "react";
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
+    // const [name, setname] = useState('hello');
+
     const [links, Setlinks] = useState([
-        {
-            id: 1,
-            content: 'home',
-            ref: '/',
-            type: 'high'
-        },
-        {
-            id: 2,
-            content: 'about', 
-            ref: "/about",
-            type: 'norm'
-        },
-        {
-            id: 3,
-            content: 'contact',
-            ref: "/contact",
-            type: 'norm'
-        },        
+        {   id: 1, content: 'home', ref: '/', type: 'norm'},
+        {   id: 2, content: 'about', ref: '/about', type: 'norm'},
+        {   id: 3, content: 'contact', ref: '/contact', type: 'norm'},        
     ]);
 
+    useEffect(
+        ()=>{
+            handleClick();
+    },[]);
 
+    const handleClick = function(){
+        const url = window.location.href;
+
+        var copy_links = links.filter(
+            (link)=>{
+                if(url.endsWith(link.ref)){
+                    link.type='high';
+                }
+                else{
+                    link.type='norm';
+                }
+                return true;
+            }
+        );
+        Setlinks(copy_links);
+    }
 
     return ( 
         <nav className="Navbar">
-            <div id="nav-heading"><a href="/">Shrey Gupta</a></div>
+            <div id={"nav-heading"}><a href="/">Shrey Gupta</a></div>
 
-            <div className="nav-links">
+            {/* <button onClick={handleClick}>{name}</button> */}
+
+            <div className="nav-links" onClick={handleClick}>
                 {
                     links.map(link => (
-                        <a href={link.ref} className={'nav-link-'+link.type} key={link.id}>
+                        <Link to={link.ref} className={'nav-link-'+link.type} key={link.id}>
                             {link.content}
-                        </a>
+                        </Link>
                     ))
                 }
             </div>
