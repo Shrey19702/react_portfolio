dd2e409fec29e28fc011a07645afce799c672392
shrey19702 <shrey19702@gmail.com>
2021-12-13 22:49:05 +0530

    changes in files (homejs, css)

--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,18 @@ const Home = () => {
                 <Navbar/>
                 <HeroPage heroTitle={heroTitle} heroClass={heroClass} heroIntro={heroIntro}/>
             </div>
-            <Projects/>
+            <Projects className={"movies-app"} 
+                text1= {'01'}
+                text2={'Movie App'}
+                text3={'A Web App to search for your favorite movies'}
+                text4={'https://movie-app-react-js.herokuapp.com/'}
+            />
+            <Projects className={"task-tracker"}
+                text1={'02'}
+                text2={'Task Tracker'}
+                text3={'A web App to track your task to be done'}
+                text4={'https://shrey19702.github.io/Task-Tracker-react-app/'}
+            />
         </Fragment>
     );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,27 @@
-const Projects = () => {
+// import movie_app from "../images/movie-app.png";
+
+const Projects = (props) => {
     return (  
-        <div className="projects-page">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In amet inventore omnis fugit quia nesciunt, facilis vel molestias praesentium quam veritatis similique soluta vitae delectus dolores necessitatibus earum nostrum iure explicabo reiciendis repellat adipisci. Sapiente fugit aliquam autem! Accusantium et quod nesciunt nobis iusto aperiam vero perferendis magnam delectus architecto? Sunt officia amet repudiandae? Vitae corporis modi neque tempore, dolore harum ipsum eius, iure qui excepturi dolorem et non veritatis exercitationem consequuntur maxime consectetur. Ipsa soluta impedit quam possimus ratione explicabo quas repellat odio et aut vero ducimus recusandae voluptatem in at nisi voluptas ullam reprehenderit mollitia aspernatur quisquam, obcaecati dignissimos accusamus suscipit! Sapiente nobis, dolore libero omnis ab aliquam dignissimos.
+        <div className={"projects-page "+(props.className)}>
+            {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. In amet inventore omnis fugit quia nesciunt, facilis vel molestias praesentium quam veritatis similique soluta vitae delectus dolores necessitatibus earum nostrum iure explicabo reiciendis repellat adipisci. Sapiente fugit aliquam autem! Accusantium et quod nesciunt nobis iusto aperiam vero perferendis magnam delectus architecto? Sunt officia amet repudiandae? Vitae corporis modi neque tempore, dolore harum ipsum eius, iure qui excepturi dolorem et non veritatis exercitationem consequuntur maxime consectetur. Ipsa soluta impedit quam possimus ratione explicabo quas repellat odio et aut vero ducimus recusandae voluptatem in at nisi voluptas ullam reprehenderit mollitia aspernatur quisquam, obcaecati dignissimos accusamus suscipit! Sapiente nobis, dolore libero omnis ab aliquam dignissimos. */}
+            {/* <img className="project-image" src={movie_app} alt="movie-app"/>             */}
+            <div className="project-page-panel">
+                <div className="project-page-panel-text-box">
+                    <div className="font-weight-400">
+                        {props.text1}
+                    </div>
+                    <div className="font-weight-900">
+                        {props.text2}
+                    </div> 
+                    <div className="font-weight-400 font-size-1-5rem">
+                        {props.text3}
+                    </div>
+                    <br/>
+                    <div className="font-weight-400 font-size-1-5rem">
+                       <a href={props.text4} className="project-link" target="_blank">{props.text2}</a> 
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
